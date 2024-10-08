45a87b340e9d77884bb5f7ef7d1ea663068ea837
Gilbike <marton.dikacz@gmail.com>
2024-10-08 08:38:20 +0200

    feladat kész

--- f1/f1.js
+++ f1/f1.js
@@ -1,7 +1,16 @@
+const APIAdress = "https://archive-api.open-meteo.com/v1/archive?";
+async function f1(lat, long, timeStr) {
+  const response = await fetch(`${APIAdress}latitude=${lat}&longitude=${long}&start_date=${timeStr}&end_date=${timeStr}&hourly=temperature_2m`);
+  const data = await response.json();
 
-const APIAdress = 'https://archive-api.open-meteo.com/v1/archive?';
-async function f1 (lat, long, timeStr) {
-    //TODO
+  const weather = data.hourly.temperature_2m;
+  let sum = 0;
+
+  weather.forEach((element) => {
+    sum += element;
+  });
+
+  return Math.floor(sum / weather.length);
 }
 
 module.exports = f1;
